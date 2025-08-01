6f345e66363d62c21ec90402dbb6e80e5bac68bc
真芙辣 <140885477+yangFenTuoZi@users.noreply.github.com>
2025-08-01 21:09:39 +0800

    更新Gradle、AGP

--- term/src/main/java/jackpal/androidterm/TermService.java
+++ term/src/main/java/jackpal/androidterm/TermService.java
@@ -94,13 +94,14 @@ public class TermService extends Service implements TermSession.FinishCallback
         mTermSessions = new SessionList();
 
         /* Put the service in the foreground. */
-        Notification notification = new Notification(R.drawable.ic_stat_service_notification_icon, getText(R.string.service_notify_text), System.currentTimeMillis());
-        notification.flags |= Notification.FLAG_ONGOING_EVENT;
-        Intent notifyIntent = new Intent(this, Term.class);
-        notifyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notifyIntent, 0);
-        notification.setLatestEventInfo(this, getText(R.string.application_terminal), getText(R.string.service_notify_text), pendingIntent);
-        compat.startForeground(RUNNING_NOTIFICATION, notification);
+        // TODO fix this to use NotificationChannel on API Level >= 26
+//        Notification notification = new Notification(R.drawable.ic_stat_service_notification_icon, getText(R.string.service_notify_text), System.currentTimeMillis());
+//        notification.flags |= Notification.FLAG_ONGOING_EVENT;
+//        Intent notifyIntent = new Intent(this, Term.class);
+//        notifyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notifyIntent, 0);
+//        notification.setLatestEventInfo(this, getText(R.string.application_terminal), getText(R.string.service_notify_text), pendingIntent);
+//        compat.startForeground(RUNNING_NOTIFICATION, notification);
 
         Log.d(TermDebug.LOG_TAG, "TermService started");
         return;
